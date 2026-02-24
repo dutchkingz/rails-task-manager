c56d941fef320fb8e05f01ab9b4866a7ef6de4c2
vincent mohanna <vince.mohanna@gmail.com>
2026-02-24 15:28:33 +0100

    logic and flow working for task list in rails

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -17,6 +17,18 @@ class TasksController < ApplicationController
     redirect_to task_path(@task)
   end
 
+  def update
+    @task = Task.find(params[:id])
+    @task.update(task_params)
+    redirect_to task_path(@task)
+  end
+
+  def destroy
+    @task = Task.find(params[:id])
+    @task.destroy
+    redirect_to tasks_path, status: :see_other
+  end
+
   private
 
   def task_params
