46e9323311517f6c6f1449125e002f19dfc7a4c6
vincent mohanna <vince.mohanna@gmail.com>
2026-02-24 14:48:31 +0100

    building the task list app part 1

--- config/routes.rb
+++ config/routes.rb
@@ -11,5 +11,7 @@ Rails.application.routes.draw do
 
   # Defines the root path rout
   get "tasks", to: "tasks#index"
-  get "tasks/:id", to: "tasks#show"
+  get "tasks/new", to: "tasks#new", as: :new_task
+  post "tasks", to: "tasks#create"
+  get "tasks/:id", to: "tasks#show", as: :task
 end
